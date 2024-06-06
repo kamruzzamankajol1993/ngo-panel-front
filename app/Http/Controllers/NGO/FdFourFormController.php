e119129db6d87603ec6344968a5500113052d76b
Musa Technologies LTD <113629203+musatechnologiesltd@users.noreply.github.com>
2024-06-06 18:31:40 +0600

    fd four working

--- app/Http/Controllers/NGO/FdFourFormController.php
+++ app/Http/Controllers/NGO/FdFourFormController.php
@@ -4,8 +4,97 @@ namespace App\Http\Controllers\NGO;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Crypt;
+use DB;
+use PDF;
+use Mpdf\Mpdf;
+use DateTime;
+use DateTimezone;
+use Response;
+use App\Http\Controllers\NGO\CommonController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Session;
+use App\Models\FormNoFour;
+use App\Models\FormNoFourSectorDetail;
+use App\Models\FdOneForm;
+use App\Models\FdFourForm;
+use App\Models\NgoRenewInfo;
+use App\Models\NgoDuration;
 class FdFourFormController extends Controller
 {
-    //
+    public function addFdFourFormData($id){
+        try{
+
+            $decodeId = base64_decode($id);
+            $checkNgoTypeForForeginNgo = DB::table('ngo_type_and_languages')
+            ->where('user_id',Auth::user()->id)->value('ngo_type');
+            $ngo_list_all = FdOneForm::where('user_id',Auth::user()->id)->first();
+
+
+            $ngoDurationReg = NgoDuration::where('fd_one_form_id',$ngo_list_all->id)->value('ngo_duration_start_date');
+        $ngoDurationLastEx = NgoDuration::where('fd_one_form_id',$ngo_list_all->id)->orderBy('id','desc')->first();
+        $renewWebsiteName = NgoRenewInfo::where('fd_one_form_id',$ngo_list_all->id)->value('web_site_name');
+
+            $fdFourFormList = FdFourForm::where('fd_one_form_id',$ngo_list_all->id)
+            ->where('fd_four_one_form_id',$decodeId)
+            ->first();
+
+            return view('front.fdFourForm.create',compact('renewWebsiteName','ngoDurationLastEx','ngoDurationReg','decodeId','ngo_list_all','fdFourFormList'));
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('error_404');
+        }
+    }
+
+    public function store(Request $request){
+
+        //dd($request->all());
+
+
+        $request->validate([
+
+            'prokolpo_name' => 'required|string',
+            'ngo_website' => 'required|string',
+            'prokolpo_duration_one' => 'required|string',
+            'exam_time' => 'required|string',
+            'start_balance' => 'required|string',
+            'foreign_grant_taken_exam_year' => 'required|string',
+            'foreign_grant_cost_exam_year' => 'required|string',
+            'foreign_grant_remaining_exam_year' => 'required|string',
+        ]);
+
+        try{
+            $ngo_list_all = FdOneForm::where('user_id',Auth::user()->id)->first();
+
+            $fdFourOneForm = new FdFourForm();
+            $fdFourOneForm->status = 'Review';
+            $fdFourOneForm->file_last_check_date = Date('Y-m-d', strtotime('+3 days'));
+            $fdFourOneForm->prokolpo_name = $request->prokolpo_name;
+            $fdFourOneForm->ngo_name = $request->ngo_name;
+            $fdFourOneForm->registration_number = $request->registration_number;
+            $fdFourOneForm->ngo_telephone = $request->ngo_telephone;
+            $fdFourOneForm->ngo_email = $request->ngo_email;
+            $fdFourOneForm->ngo_website = $request->ngo_website;
+            $fdFourOneForm->prokolpo_duration_one = $request->prokolpo_duration_one;
+            $fdFourOneForm->exam_time =$request->exam_time;
+            $fdFourOneForm->start_balance =$request->start_balance;
+            $fdFourOneForm->foreign_grant_taken_exam_year =$request->foreign_grant_taken_exam_year;
+            $fdFourOneForm->foreign_grant_remaining_exam_year =$request->foreign_grant_remaining_exam_year;
+            $fdFourOneForm->fd_one_form_id = $ngo_list_all->id;
+            $fdFourOneForm->fd_four_one_form_id = $request->decodeId;
+            $fdFourOneForm->save();
+
+
+            $fdFourOneFormId = $fdFourOneForm->id;
+
+
+            return redirect()->route('fdFourOneForm.show',base64_encode($request->decodeId))->with('success','Added Successfully');
+
+        } catch (\Exception $e) {
+
+            return redirect()->route('error_404');
+        }
+    }
 }
--- app/Http/Controllers/NGO/FdFourOneController.php
+++ app/Http/Controllers/NGO/FdFourOneController.php
@@ -4,8 +4,159 @@ namespace App\Http\Controllers\NGO;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Crypt;
+use DB;
+use PDF;
+use Mpdf\Mpdf;
+use DateTime;
+use DateTimezone;
+use Response;
+use App\Http\Controllers\NGO\CommonController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Session;
+use App\Models\FormNoFour;
+use App\Models\FormNoFourSectorDetail;
+use App\Models\FdFourOneExpenditureSector;
+use App\Models\FdOneForm;
+use App\Models\FdFourOneForm;
+use App\Models\FdFourForm;
 class FdFourOneController extends Controller
 {
-    //
+    public function index(){
+        try{
+            $checkNgoTypeForForeginNgo = DB::table('ngo_type_and_languages')
+            ->where('user_id',Auth::user()->id)->value('ngo_type');
+            $ngo_list_all = FdOneForm::where('user_id',Auth::user()->id)->first();
+            $fdFourOneFormList = FdFourOneForm::where('fd_one_form_id',$ngo_list_all->id)
+            ->latest()->get();
+
+            return view('front.fdFourOneForm.index',compact('ngo_list_all','fdFourOneFormList'));
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('error_404');
+        }
+
+
+
+    }
+
+
+    public function show(){
+        try{
+
+            $decode_id = base64_decode($id);
+
+            $checkNgoTypeForForeginNgo = DB::table('ngo_type_and_languages')
+            ->where('user_id',Auth::user()->id)->value('ngo_type');
+            $ngo_list_all = FdOneForm::where('user_id',Auth::user()->id)->first();
+
+            $fdFourOneFormList = FdFourOneForm::where('id',$decode_id)
+            ->latest()->first();
+
+
+            $fdFourFormList = FdFourForm::where('fd_four_one_form_id',$decode_id)
+            ->latest()->first();
+
+            $fdFourOneFormExpenditorSector = FdFourOneExpenditureSector::where('fd_four_one_id',$decode_id)->latest()->get();
+
+
+
+
+            return view('front.fdFourOneForm.show',compact('fdFourOneFormExpenditorSector','fdFourFormList','ngo_list_all','fdFourOneFormList'));
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('error_404');
+        }
+
+
+
+    }
+
+    public function create(){
+
+        try{
+
+            $checkNgoTypeForForeginNgo = DB::table('ngo_type_and_languages')->where('user_id',Auth::user()->id)->value('ngo_type');
+            $ngo_list_all = FdOneForm::where('user_id',Auth::user()->id)->first();
+
+            $districtList = DB::table('civilinfos')->groupBy('district_bn')
+            ->select('district_bn')->get();
+
+            return view('front.fdFourOneForm.create',compact('ngo_list_all','districtList'));
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('error_404');
+        }
+    }
+
+
+    public function store(Request $request){
+
+        //dd($request->all());
+
+        $request->validate([
+
+            'prokolpo_name' => 'required|string',
+            'prokolpo_permission_sarok_no' => 'required|string',
+            'prokolpo_permission_sarok_date' => 'required|string',
+            'prokolpo_year' => 'required|string',
+            'prokolpo_amount_sarkrito_bangla_amount' => 'required|string',
+            'prokolpo_amount_grihito' => 'required|string',
+            'prokolpo_amount_grihito_date' => 'required|string',
+            'prokolpo_amount_sarkrito_date' => 'required|string',
+        ]);
+
+        try{
+            $ngo_list_all = FdOneForm::where('user_id',Auth::user()->id)->first();
+
+            $fdFourOneForm = new FdFourOneForm();
+            $fdFourOneForm->status = 'Review';
+            $fdFourOneForm->file_last_check_date = Date('Y-m-d', strtotime('+3 days'));
+            $fdFourOneForm->prokolpo_name = $request->prokolpo_name;
+            $fdFourOneForm->prokolpo_permission_sarok_no = $request->prokolpo_permission_sarok_no;
+            $fdFourOneForm->prokolpo_permission_sarok_date = $request->prokolpo_permission_sarok_date;
+            $fdFourOneForm->prokolpo_year = $request->prokolpo_year;
+            $fdFourOneForm->prokolpo_amount_sarkrito_bangla_amount = $request->prokolpo_amount_sarkrito_bangla_amount;
+            $fdFourOneForm->prokolpo_amount_sarkrito_date = $request->prokolpo_amount_sarkrito_date;
+            $fdFourOneForm->prokolpo_amount_grihito = $request->prokolpo_amount_grihito;
+            $fdFourOneForm->prokolpo_amount_grihito_date =$request->prokolpo_amount_grihito_date;
+            $fdFourOneForm->fd_one_form_id = $ngo_list_all->id;
+            $fdFourOneForm->save();
+
+
+            $fdFourOneFormId = $fdFourOneForm->id;
+
+            $input = $request->all();
+            $expenditure_sectors = $input['expenditure_sectors'];
+
+
+            foreach($expenditure_sectors as $key => $expenditure_sectors){
+
+                $form= new FdFourOneExpenditureSector();
+                $form->fd_four_one_id = $fdFourOneFormId;
+                $form->expenditure_sectors=$input['expenditure_sectors'][$key];
+                $form->approved_budget_money=$input['approved_budget_money'][$key];
+                $form->actual_cost=$input['actual_cost'][$key];
+                $form->difference=$input['difference'][$key];
+                $form->percentage=$input['percentage'][$key];
+                $form->reason_for_the_difference=$input['reason_for_the_difference'][$key];
+                $form->save();
+            }
+
+
+
+
+
+            return redirect()->route('addFdFourFormData',base64_encode($fdFourOneFormId))->with('success','Added Successfully');
+
+        } catch (\Exception $e) {
+
+            return redirect()->route('error_404');
+        }
+    }
+
 }
